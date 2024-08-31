b5babc394e5d5e2ba755b4091dd18a44b6581dd4
Aissutievos <Traviz1232@gmail.com>
2024-08-31 15:25:04 -0400

    Fix RainCycle_ctor hook

--- src/RegionProperties/CycleRelatedHooks.cs
+++ src/RegionProperties/CycleRelatedHooks.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using CustomRegions.Mod;
+using Mono.Cecil;
 using Mono.Cecil.Cil;
 using static CustomRegions.RegionProperties.RegionProperties;
 
@@ -57,7 +58,7 @@ namespace CustomRegions.RegionProperties
                 x => x.MatchCallvirt(out _),
                 x => x.MatchBrfalse(out _),
                 x => x.MatchLdcR4(0),
-                x => x.MatchLdloc(out index)
+                x => x.MatchStloc(out index)
                 ))
             {
                 c.Emit(OpCodes.Ldloc, index);
@@ -79,7 +80,7 @@ namespace CustomRegions.RegionProperties
                     if (self.world.region?.regionParams.earlyCycleChance == 0f) return 0f;
                     return orig;
                 });
-                c.Emit(OpCodes.Ldloc, index);
+                c.Emit(OpCodes.Stloc, index);
             }
             else
             {
